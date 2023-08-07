607c5556ca8fc806c37a19f8cb3ee7c8b5adaea8
unknown <imthelaw115@gmail.com>
2023-08-07 12:05:27 +0200

    aled

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Faker\Factory;
 use Faker\Generator;
 use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -22,16 +24,46 @@ class ArticleFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        for($i=1; $i <= 15; $i++)
+        for($j = 1; $j <= 3 ; $j++  )
         {
-            $article = new Article;
+            $category = new Category;
+            $category->setTitle($this->faker->sentence(1))
+                    ->setDescription($this->faker->paragraph());
+            $manager->persist($category);
 
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setImage("https://picsum.photos/200/300")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setCreatedAt(new \Datetime());
-                    
-            $manager->persist($article);
+            
+            for($i=1; $i <= mt_rand(8, 12); $i++)
+            {
+                $article = new Article;
+    
+                $article->setTitle($this->faker->sentence())
+                        ->setImage($this->faker->imageUrl())
+                        ->setContent($this->faker->paragraph(200))
+                        ->setCreatedAt($this->faker->dateTimeBetween('-10 months'))
+                        ->setCategory($category);
+                        
+                $manager->persist($article);
+
+                for($k= 1; $k <= mt_rand(4, 10); $k++)
+                {
+                    $comment = new Comment;
+
+                    //* on récupère la date d'aujourd'hui
+                    $now = new \DateTime();
+                    //* on fait la différence entre la date d'aujourd'hui et la date de création de notre article
+                    $interval = $now->diff($article->getCreatedAt());
+                    //* on récupère la différence en nombre de jours
+                    $days = $interval->days;
+                    $minimum = '-' . $days . ' days';
+
+                    $comment->setAuthor($this->faker->name)
+                            ->setContent($this->faker->paragraph())
+                            ->setArticle($article)
+                            ->setCreatedAt($this->faker->dateTimeBetween($minimum));
+                    $manager->persist($comment);
+                }
+
+            }
         }
         // $product = new Product();
         // $manager->persist($product);
